b8cb1da3f4eb53f6ce90b6dfa90c6e2907c3ec88
Swapnil Godambe <swapnil.g@servify.in>
2020-09-29 13:27:06 +0530

    completes codelab

--- 01-3-image-resources-and-compatibility/app/src/main/java/com/swapnull/diceroller/MainActivity.kt
+++ 01-3-image-resources-and-compatibility/app/src/main/java/com/swapnull/diceroller/MainActivity.kt
@@ -15,10 +15,20 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun rollDice() {
-        val randomInt = (1..6).random()
 
-        
+        var randomInt = (1..6).random()
+
+        val drawableResource = when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        imgDice.setImageResource(drawableResource)
+
     }
-
 
 }
