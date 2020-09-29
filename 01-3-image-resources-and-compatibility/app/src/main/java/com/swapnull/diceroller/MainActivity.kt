2a479de9e333681c9bcf5e52a138222f0cf664fa
Swapnil Godambe <swapnil.g@servify.in>
2020-09-29 13:15:54 +0530

    base app

---
+++ 01-3-image-resources-and-compatibility/app/src/main/java/com/swapnull/diceroller/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.swapnull.diceroller
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btnRoll.setOnClickListener { rollDice() }
+    }
+
+    private fun rollDice() {
+        val randomInt = (1..6).random()
+
+        
+    }
+
+
+}
