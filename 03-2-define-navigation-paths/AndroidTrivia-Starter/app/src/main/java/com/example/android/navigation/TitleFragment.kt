4e152a895a527de14fc51e11c38e53e0ff80f63f
Swapnil Godambe <swapnil.g@servify.in>
2020-10-08 10:21:53 +0530

    Task: Add fragments to the navigation graph

--- 03-2-define-navigation-paths/AndroidTrivia-Starter/app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ 03-2-define-navigation-paths/AndroidTrivia-Starter/app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
 import com.example.android.navigation.databinding.FragmentTitleBinding
 
 class TitleFragment : Fragment() {
@@ -14,9 +15,11 @@ class TitleFragment : Fragment() {
 
         val binding = DataBindingUtil.inflate<FragmentTitleBinding>(inflater, R.layout.fragment_title, container, false)
 
+        binding.playButton.setOnClickListener() { view: View ->
+            view.findNavController().navigate(R.id.action_titleFragment_to_gameFragment)
+        }
+
         return binding.root
     }
-
-
 
 }
