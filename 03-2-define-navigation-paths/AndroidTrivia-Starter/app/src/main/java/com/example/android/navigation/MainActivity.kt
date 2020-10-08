a29fae2f51a070dec63bc83ea345fe01e91e642c
Swapnil Godambe <swapnil.g@servify.in>
2020-10-08 13:02:42 +0530

    Task: Add an Up button in the app bar

--- 03-2-define-navigation-paths/AndroidTrivia-Starter/app/src/main/java/com/example/android/navigation/MainActivity.kt
+++ 03-2-define-navigation-paths/AndroidTrivia-Starter/app/src/main/java/com/example/android/navigation/MainActivity.kt
@@ -19,6 +19,8 @@ package com.example.android.navigation
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.example.android.navigation.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -26,6 +28,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         @Suppress("UNUSED_VARIABLE")
         val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+
+        val navController = this.findNavController(R.id.myNavHostFragment)
+        NavigationUI.setupActionBarWithNavController(this, navController)
+
     }
 
     // TODO (01) Create the new TitleFragment
@@ -37,4 +43,10 @@ class MainActivity : AppCompatActivity() {
     // TODO (03) Use DataBindingUtil.inflate to inflate and return the titleFragment in onCreateView
     // In our new TitleFragment
     // R.layout.fragment_title
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = this.findNavController(R.id.myNavHostFragment)
+        return navController.navigateUp()
+    }
+
 }
