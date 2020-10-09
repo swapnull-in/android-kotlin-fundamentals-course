9305a4ac2165661e112543f0ba1ecd24bcec096a
Swapnil Godambe <swapnil.g@servify.in>
2020-10-09 13:56:55 +0530

    Task: Set up and use the Safe Args plugin

--- 03-3-start-an-external-activity/AndroidTriviaNavigation/app/src/main/java/com/example/android/navigation/GameFragment.kt
+++ 03-3-start-an-external-activity/AndroidTriviaNavigation/app/src/main/java/com/example/android/navigation/GameFragment.kt
@@ -101,7 +101,7 @@ class GameFragment : Fragment() {
                     } else {
                         // We've won!  Navigate to the gameWonFragment.
                         view.findNavController()
-                                .navigate(GameFragmentDirections.actionGameFragmentToGameWonFragment())
+                                .navigate(GameFragmentDirections.actionGameFragmentToGameWonFragment(numQuestions, questionIndex))
                     }
                 } else {
                     // Game over! A wrong answer sends us to the gameOverFragment.
