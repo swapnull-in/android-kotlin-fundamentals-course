5cf19295ca4c3bc78fc9be0761c7f3b17202f2a6
Swapnil Godambe <swapnil.g@servify.in>
2020-10-13 16:20:03 +0530

    Task: Simulate app shutdown and use onSaveInstanceState()

--- 04-2-complex-lifecycle-situations/DessertClickerLogs/app/src/main/java/com/example/android/dessertclicker/MainActivity.kt
+++ 04-2-complex-lifecycle-situations/DessertClickerLogs/app/src/main/java/com/example/android/dessertclicker/MainActivity.kt
@@ -28,6 +28,10 @@ import androidx.databinding.DataBindingUtil
 import com.example.android.dessertclicker.databinding.ActivityMainBinding
 import timber.log.Timber
 
+const val KEY_REVENUE = "revenue_key"
+const val KEY_DESSERT_SOLD = "dessert_sold_key"
+const val KEY_TIMER_SECONDS = "timer_seconds_key"
+
 class MainActivity : AppCompatActivity() {
 
     private var revenue = 0
@@ -69,6 +73,16 @@ class MainActivity : AppCompatActivity() {
 
         Timber.i("onCreate called")
 
+        dessertTimer = DessertTimer(this.lifecycle)
+
+        if (savedInstanceState != null) {
+            revenue = savedInstanceState.getInt(KEY_REVENUE, 0)
+            dessertsSold = savedInstanceState.getInt(KEY_DESSERT_SOLD, 0)
+            dessertTimer.secondsCount =
+                    savedInstanceState.getInt(KEY_TIMER_SECONDS, 0)
+            showCurrentDessert()
+        }
+
         // Use Data Binding to get reference to the views
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
@@ -80,10 +94,14 @@ class MainActivity : AppCompatActivity() {
         binding.revenue = revenue
         binding.amountSold = dessertsSold
 
-        dessertTimer = DessertTimer(this.lifecycle)
-
         // Make sure the correct dessert is showing
         binding.dessertButton.setImageResource(currentDessert.imageId)
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+
+        Timber.i("onSaveInstanceState Called")
     }
 
     /**
