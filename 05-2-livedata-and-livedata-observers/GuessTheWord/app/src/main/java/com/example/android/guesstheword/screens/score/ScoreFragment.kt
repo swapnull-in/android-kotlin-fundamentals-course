89db769364420fa137e7a3c34b03742626ad5d26
Swapnil Godambe <swapnil.g@servify.in>
2020-10-15 09:24:57 +0530

    Task: Add LiveData to the ScoreViewModel

--- 05-2-livedata-and-livedata-observers/GuessTheWord/app/src/main/java/com/example/android/guesstheword/screens/score/ScoreFragment.kt
+++ 05-2-livedata-and-livedata-observers/GuessTheWord/app/src/main/java/com/example/android/guesstheword/screens/score/ScoreFragment.kt
@@ -22,6 +22,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.navArgs
@@ -54,7 +55,9 @@ class ScoreFragment : Fragment() {
         viewModel = ViewModelProvider(this, viewModelFactory)
                 .get(ScoreViewModel::class.java)
 
-        binding.scoreText.text = viewModel.score.toString()
+        viewModel.score.observe(viewLifecycleOwner, Observer { newScore ->
+            binding.scoreText.text = newScore.toString()
+        })
 
         return binding.root
     }
--- 05-2-livedata-and-livedata-observers/GuessTheWord/app/src/main/java/com/example/android/guesstheword/screens/score/ScoreViewModel.kt
+++ 05-2-livedata-and-livedata-observers/GuessTheWord/app/src/main/java/com/example/android/guesstheword/screens/score/ScoreViewModel.kt
@@ -1,14 +1,21 @@
 package com.example.android.guesstheword.screens.score
 
 import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class ScoreViewModel(finalScore: Int) : ViewModel() {
 
     // The final score
-    var score = finalScore
+    private val _score = MutableLiveData<Int>()
+    val score: LiveData<Int>
+        get() = _score
 
     init {
+
+        _score.value = finalScore
+
         Log.i("ScoreViewModel", "Final score is $finalScore")
     }
 
